de2bfa7d6a1a24f6e523e71662fed62f71ccff4a
Tu Nombre <you@example.com>
2022-11-06 23:51:02 -0500

    as

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -32,10 +32,10 @@ class Database extends Config
      */
     public $default = [
         'DSN'      => '',
-        'hostname' => 'localhost',
-        'username' => '',
-        'password' => '',
-        'database' => '',
+        'hostname' => '',
+        'username' => 'root',
+        'password' => 'PAZZ2597',
+        'database' => 'restaurante',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
