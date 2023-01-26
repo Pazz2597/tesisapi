b8efc4ae0e146bd7b02652eab6f3d64339598ede
cmruizg777 <44457004+cmruizg777@users.noreply.github.com>
2023-01-25 22:41:55 -0500

    websockets

--- app/Config/Services.php
+++ app/Config/Services.php
@@ -1,32 +1,35 @@
-<?php
-
+<?php 
 namespace Config;
 
+use App\Libraries\CodeigniterWebsocket as LibrariesCodeigniterWebsocket;
 use CodeIgniter\Config\BaseService;
+use CodeIgniter\Config\BaseConfig;
+
 
 /**
- * Services Configuration file.
+ * @package   CodeIgniter WebSocket Library: Default config service
+ * @category  Libraries
+ * @author    Taki Elias <taki.elias@gmail.com>
+ * @license   http://opensource.org/licenses/MIT > MIT License
+ * @link      https://github.com/takielias
  *
- * Services are simply other classes/libraries that the system uses
- * to do its job. This is used by CodeIgniter to allow the core of the
- * framework to be swapped out easily without affecting the usage within
- * the rest of your application.
- *
- * This file holds any application-specific services, or service overrides
- * that you might need. An example has been included with the general
- * method format you should use for your service methods. For more examples,
- * see the core Services file at system/Config/Services.php.
+ * CodeIgniter WebSocket library. It allows you to make powerful realtime applications by using Ratchet Websocket
  */
+
 class Services extends BaseService
 {
-    /*
-     * public static function example($getShared = true)
-     * {
-     *     if ($getShared) {
-     *         return static::getSharedInstance('example');
-     *     }
-     *
-     *     return new \CodeIgniter\Example();
-     * }
-     */
+    public static function CodeigniterWebsocket(BaseConfig $config = null, bool $getShared = true)
+    {
+        output('info', 'Hola !');
+        if ($getShared):
+            return static::getSharedInstance('CodeigniterWebsocket', $config);
+        endif;
+
+        // If no config was injected then load one
+        // Prioritizes app/Config if found
+        if (empty($config))
+            $config = config('CodeigniterWebsocket');
+
+        return new LibrariesCodeigniterWebsocket($config);
+    }
 }
